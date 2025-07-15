0d2285dc3d697cc1086e87712c76c03c08660b09
prakash4600 <114327234+prakash4600@users.noreply.github.com>
2025-07-15 11:35:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
-  features {}
-
-  subscription_id = "AZURE_SUBSCRIPTION_ID"
-  client_id       = "AZURE_CLIENT_ID"
-  client_secret   = "AZURE_CLIENT_SECRET"
-  tenant_id       = "AZURE_TENANT_ID"
+  features {
 }
 
 resource "azurerm_resource_group" "example" {
