57276fcefbad15dc4eaa77e31dbb2101664db8b1
prakash4600 <114327234+prakash4600@users.noreply.github.com>
2025-07-15 11:38:58 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
